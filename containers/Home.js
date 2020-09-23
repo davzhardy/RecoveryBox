6d8afc954ab3ef657d44fe714c185b5cccbb62c2
David Hardy <david.hardy50@gmail.com>
2020-09-23 15:31:03 +0200

    feat: additional structural components and containers

--- containers/Home.js
+++ containers/Home.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import {useDispatch, useSelector} from "react-redux";
+import { BoldAppText, MediumAppText } from '../styles/text'
+import { DateTime } from "luxon";
 
 function HomeScreen () {
+
+  const now = DateTime.local()
+  const dayFormat = ''
+  if (now.day === 1)
+  dayFormat = 'st'
+  else if (now.day === 2)
+  dayFormat = 'nd' 
+  else if (now.day === 3)
+  dayFormat = 'rd'
+  else dayFormat = 'th' 
 
   const username = useSelector((state) => state.user.username)
 
