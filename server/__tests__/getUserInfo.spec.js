f894272aa9774f29826c16bcba4f9318faceffa4
Raphael <raphael.mazet@gmail.com>
2020-10-05 13:54:48 +0100

    test: refactor utils folders server side

--- server/__tests__/getUserInfo.spec.js
+++ server/__tests__/getUserInfo.spec.js
@@ -1,20 +1,20 @@
 const { getUserInfo } = require ('../controller');
 const db = require('../models/index');
-const { mockRequest, mockResponse } = require('../util/interceptor');
+const { mockRequest, mockResponse } = require('../util/testUtils/interceptor');
+const { mockGetUserInfoResponse } = require('../util/testUtils/mockDbResponses/mockDbResponses');
 
 jest.mock('../models/index');
-
-const mockDbResponse = 'mockDbResponse';
 
 //TODO: will need to implement a 404 test on the router if no params to this route
 describe("getUserInfo controller function", () => {
   test("should 200 and return all data for a specific user from the DB", async () => {
-    let req = mockRequest();
+    const req = mockRequest();
+    const res = mockResponse();
+    const mockDbResponse = mockGetUserInfoResponse;
     req.params.username = 'Raph';
     db.User.findAll.mockResolvedValue(mockDbResponse);
-    const res = mockResponse();
 
-    await getUserInfo(req, res);
+    const response = await getUserInfo(req, res);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.send.mock.calls.length).toBe(1);
