e1452abf8482a94e71695d0a13af41a65d97288f
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2020-10-06 19:37:46 +0100

    authentication: serverside jwt authentication after verifying google oAuth

--- server/controllers/oAuthController.js
+++ server/controllers/oAuthController.js
@@ -1,22 +1,49 @@
-const {OAuth2Client} = require('google-auth-library');
 require('dotenv').config();
+const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID); 
+const jwt = require('jsonwebtoken');
+const JWT_KEY = process.env.JWT_KEY;
+const db = require('../models/index');
 
+const login = async (req, res) => {
+  const { email, } = req.body;
+  try {
+		
+    if (!validatedPass) throw new Error();
+    
+    
+  } catch (error) {
+    res
+      .status(401)
+      .send({ error: '401', message: 'Username or password is incorrect' });
+  }
+  // REMOVE-END
+};
 
 async function googleVerify(req, res) {
-
-	console.log('req:', req.body)
-	const { idToken } = req.body; //REL WHY IS NOT DEFINED?
-  const ticket = await client.verifyIdToken({
-      idToken: idToken,
-      audience: process.env.IOS_CLIENT_ID,
-  });
-  const payload = ticket.getPayload();
-  console.log('payload:', payload)
-  const userid = payload['sub'];
-  console.log('userid:', userid)
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
+	try {
+		console.log('req:', req.body)
+		const { idToken } = req.body; //REL WHY IS NOT DEFINED?
+		const ticket = await client.verifyIdToken({
+				idToken: idToken,
+				audience: process.env.IOS_CLIENT_ID,
+		});
+		const payload = ticket.getPayload();
+		console.log('payload:', payload)
+		const userEmail = payload['email'];
+		//Lookup the user using the info we have
+		
+		const user = await db.User.findAll({
+			where: { email: `${userEmail}`}})
+		console.log(' ---> user', user);
+			const accessToken = jwt.sign({ id: user.id }, JWT_KEY);
+			res.status(200).send({ accessToken });
+	} catch (error) {
+    res
+      .status(401)
+      .send({ error: '401', message: 'Username or password is incorrect' });
+  }
+	//TODO: compare aud from the payload === client_ID
 }
 
 
